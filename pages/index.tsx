499931428bcd185e795ab1cb32404b77d4f8bb0e
Alexis <contact@alexis-petit.fr>
2023-06-14 14:36:25 +0200

    env prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bonjour <a href="https://nextjs.org">Watchtower!</a>
+          Yo <a href="https://nextjs.org">Watchtower!</a>
         </h1>
 
         <p className={styles.description}>
